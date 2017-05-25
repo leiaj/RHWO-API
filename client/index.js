fa74235d75111e0ad6607af485023ccac7ebd68f
kylewstewart <kyle.w.stewart@mac.com>
2017-05-25 11:11:38 -0400

    nav bar

--- client/index.js
+++ client/index.js
@@ -2,9 +2,14 @@ const url = "http://localhost:3000/api/v1/housewives"
 
 
 // API REQUESTS
-const getNames = () => { $.get(url, displayNames) }
+const getAllNames = () => { $.get(url, {franchise: "All"}, displayNames) }
+function getNamesFranchise(event) {
+  let city = event.target.innerHTML
+  $.get(url, {franchise: city }, displayNames)
+}
+
 const getDetails = (id) => { $.get(url + "/" + id, displayDetails) }
-const addNew = (serialized_form) => { $.post(url, serialized_form, displayDetails).done(getNames) }
+const addNew = (serialized_form) => { $.post(url, serialized_form, displayDetails).done(getAllNames) }
 
 
 // DISPLAY JSON DATA
@@ -12,7 +17,6 @@ function displayNames(housewives) {
   const names = housewives.map( function (housewife) {
     return nameHtml(housewife)
   })
-  names.push([addNewHtml])
   $('#housewives').html(names.join(''))
 }
 
@@ -30,8 +34,7 @@ const hideDetails = () => { $('#housewife-details-div').hide() }
 const showDetails = () => { $('#housewife-details-div').show() }
 
  // HTML Templates
-const nameHtml = (housewife) => { return `<li> <a href="#" onclick="getDetails(${housewife.id})"> <h5> ${housewife.name} </h5> </a> </li>`}
-const addNewHtml = `<li> <a href = "#" onclick="displayForm()"> <h5> Add a new housewife </h5> </a> </li>`
+const nameHtml = (housewife) => { return `<li> <a href="#" onclick="getDetails(${housewife.id})"> <p> ${housewife.name} </p> </a> </li>`}
 function housewifeDetailsHtml(housewife){
   return  `<h4> ${housewife.name} </h4>
           <p> <img src="${housewife.photo}" class="circle responsive-img"> </p>
@@ -44,7 +47,7 @@ function housewifeDetailsHtml(housewife){
 
 // Events
 $(document).ready(function () {
-  getNames()
+  getAllNames()
 
   $('#new_housewife').submit(function (event) {
     event.preventDefault()
